3f7e126d7fbe001de61b68d99983f942cc8916ba
PovilasGe <povilasgedminas@gmail.com>
2022-11-03 08:55:19 +0200

    fixed

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-function init() {
+async function init() {
     const queryParams = document.location.search;
     const urlParams = new URLSearchParams(queryParams);
     const search = urlParams.get('search');
@@ -9,106 +9,83 @@ function init() {
     searchPageTitle.textContent = `Results, search phrase: ${search}`;
   
     searchResults.append(searchPageTitle);
-    console.log(search);
+  
+    const usersRes = await fetch(`https://jsonplaceholder.typicode.com/users?q=${search}`);
+    const users = await usersRes.json();
+  
+    const postsRes = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`);
+    const posts = await postsRes.json();
+  
+    const albumsRes = await fetch(`https://jsonplaceholder.typicode.com/albums?q=${search}`);
+    const albums = await albumsRes.json()
     
-    fetch(`https://jsonplaceholder.typicode.com/users?q=${search}`)
-      .then(res => res.json())
-      .then(users => {
-        if (users.length > 0) {
-          const usersWrapper = document.createElement('div');
-          usersWrapper.classList.add('users-wrapper', 'search-result-wrapper');
-          searchResults.append(usersWrapper);
+    const formattedUsers = users.map(user => {
+      const formattedUser = {
+        id: user.id,
+        title: user.name,
+      }
   
-          const usersWrapperTitle = document.createElement('h2');
-          usersWrapperTitle.classList.add('search-wrapper-title');
-          usersWrapperTitle.textContent = 'Users:';
+      return formattedUser;
+    });
   
-          const usersList = document.createElement('ul');
-          usersList.classList.add('search-list');
+    renderSearchResults({
+      data: formattedUsers,
+      parentElement: searchResults,
+      title: 'Users',
+      path: 'user',
+    });
   
-          usersWrapper.append(usersWrapperTitle, usersList);
+    renderSearchResults({
+      data: posts,
+      parentElement: searchResults,
+      title: 'Posts',
+      path: 'post',
+    });
   
-          users.map(user => {
-            const userItem = document.createElement('li');
-            userItem.classList.add('search-item');          
+    const params = {
+      data: albums,
+      parentElement: searchResults,
+      title: 'Albums',
+      path: 'album',
+    };
   
-            const userLink = document.createElement('a');
-            userLink.textContent = user.name;
-            userLink.href = './user.html?user_id=' + user.id;
+    renderSearchResults(params);
+  }
   
-            userItem.append(userLink);
-            usersList.append(userItem);
-          })
-        } else {
-          console.log('No users...');
-        }
+  function renderSearchResults(paramsObj) {
+    let { data, parentElement, title, path } = paramsObj;
+  
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('search-result-wrapper');
+    parentElement.append(wrapper);
+  
+    const wrapperTitle = document.createElement('h2');
+    wrapperTitle.classList.add('search-wrapper-title');
+  
+    wrapper.append(wrapperTitle);
+  
+    if (data.length > 0) {
+      wrapperTitle.textContent = title + ':';
+  
+      const list = document.createElement('ul');
+      list.classList.add('search-list');
+  
+      wrapper.append(list);
+  
+      data.map(item => {
+        const itemElement = document.createElement('li');
+        itemElement.classList.add('search-list-item');
+  
+        const linkElement = document.createElement('a');
+        linkElement.textContent = item.title;
+        linkElement.href = `./${path}.html?${path}_id=${item.id}`;
+  
+        itemElement.append(linkElement);
+        list.append(itemElement);
       })
-  
-    fetch(`https://jsonplaceholder.typicode.com/posts?q=${search}`)
-      .then(res => res.json())
-      .then(posts => {
-        if (posts.length > 0) {
-          const postsWrapper = document.createElement('div');
-          postsWrapper.classList.add('posts-wrapper', 'search-result-wrapper');
-          searchResults.append(postsWrapper);
-  
-          const postsWrapperTitle = document.createElement('h2');
-          postsWrapperTitle.classList.add('search-wrapper-title');
-          postsWrapperTitle.textContent = 'Posts:';
-  
-          const postsList = document.createElement('ul');
-          postsList.classList.add('search-list');
-  
-          postsWrapper.append(postsWrapperTitle, postsList);
-  
-          posts.map(post => {
-            const postItem = document.createElement('li');
-            postItem.classList.add('search-item');
-    
-            const postLink = document.createElement('a');
-            postLink.textContent = post.title;
-            postLink.href = './post.html?post_id=' + post.id;
-    
-            postItem.append(postLink);
-            postsList.append(postItem);
-          })
-        } else {
-          console.log('No users...')
-        }
-      })
-  
-    fetch(`https://jsonplaceholder.typicode.com/albums?q=${search}`)
-      .then(res => res.json())
-      .then(albums => {
-        if (albums.length > 0) {
-          const albumsWrapper = document.createElement('div');
-          albumsWrapper.classList.add('albums-wrapper', 'search-result-wrapper');
-          searchResults.append(albumsWrapper);
-  
-          const albumsWrapperTitle = document.createElement('h2');
-          albumsWrapperTitle.classList.add('search-wrapper-title');
-          albumsWrapperTitle.textContent = 'Albums:';
-  
-          const albumsList = document.createElement('ul');
-          albumsList.classList.add('search-list');
-  
-          albumsWrapper.append(albumsWrapperTitle, albumsList);
-  
-          albums.map(album => {
-            const albumItem = document.createElement('li');
-            albumItem.classList.add('album-item');
-    
-            const albumLink = document.createElement('a');
-            albumLink.textContent = album.title;
-            albumLink.href = './album.html?album_id=' + album.id;
-  
-            albumItem.append(albumLink);
-            albumsList.append(albumItem);
-          })
-        } else {
-          console.log('No albums...')
-        }
-      })
+    } else {
+      wrapperTitle.textContent = 'No ' + title.toLowerCase() + '... :(';
+    }
   }
   
   init();
