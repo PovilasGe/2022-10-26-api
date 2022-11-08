d8f1614d498776f0d61ea1ba226c1fa636eeae2d
PovilasGe <povilasgedminas@gmail.com>
2022-11-08 16:04:34 +0200

    limit added

--- post.js
+++ post.js
@@ -1,60 +1,36 @@
-import renderHeader from './header.js';
-import { getUrlParam, renderSinglePost, renderAllComments, fetchData, createElement, renderSingleComment } from './functions.js';
+import { createLinksList, fetchData, getUrlParam, createElement } from "./functions.js";
+import renderHeader from "./header.js";
+import renderPaginationLinks from "./pagination.js";
 
 async function init() {
-  const postId = getUrlParam('post_id');
-  
-  const post = await fetchData(`https://jsonplaceholder.typicode.com/posts/${postId}?_expand=user&_embed=comments`);
-  
-  const postWrapper = document.querySelector('#post-wrapper');
-  const editPostLink = createElement('a', 'Edit Post', 'edit-post-button');
-  editPostLink.href = './edit-post.html?post_id=' + postId;
-  const postContent = renderSinglePost(post);
-  const postComments = renderAllComments(post);
-  const otherPosts = renderOtherPostsList(post);
+  const userId = getUrlParam('user_id');
+  const limit = getUrlParam('limit') ? getUrlParam('limit') : 10;
+  const page = getUrlParam('page') ? getUrlParam('page') : 1;
 
-  postWrapper.append(editPostLink, postContent, otherPosts, postComments);
+  let fetchUrl = '';
+  if (userId) {
+    fetchUrl = `https://jsonplaceholder.typicode.com/users/${userId}/posts`;
+  } else {
+    fetchUrl = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
+  }
 
+  const posts = await fetchData(fetchUrl);
+  const postsWrapper = document.querySelector('#posts-wrapper');
+
+  const pageTitle = createElement('h1', 'Posts List:', 'page-title');
+
+  const postsListElement = createLinksList({
+    data: posts,
+    path: 'post',
+    listClasses: ['posts-list'],
+    itemClasses: ['post-item']
+  });
+
+  const pagination = renderPaginationLinks({page, limit});
+
+  postsWrapper.append(pageTitle, pagination, postsListElement);
+  
   renderHeader();
-
-  const createCommentForm = document.querySelector('#create-comment-form');
-
-  createCommentForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const name = event.target.elements.name.value;
-    const email = event.target.elements.email.value;
-    const body = event.target.elements.body.value;
-
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        name,
-        email,
-        body,
-        postId
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-
-    const createdComment = await res.json();
-    const singleCommentElement = renderSingleComment(createdComment);
-
-    const commentsList = postComments.querySelector('.comments-list');
-    commentsList.append(singleCommentElement);
-
-    event.target.reset();
-  })
-}
-
-function renderOtherPostsList(post) {
-  let moreAuthorPostsElement = document.createElement('a');
-  moreAuthorPostsElement.classList.add('more-posts');
-  moreAuthorPostsElement.href = './posts.html?user_id=' + post.user.id;
-  moreAuthorPostsElement.textContent = `Other posts of ${post.user.name}`;
-  return moreAuthorPostsElement;
 }
 
 init();
