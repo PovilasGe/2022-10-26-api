89c7b41dd347f724edfc56ac6ba172c6da23e12f
PovilasGe <povilasgedminas@gmail.com>
2022-11-08 13:44:07 +0200

    inactive pagination first and last pages link

--- pagination.js
+++ pagination.js
@@ -8,6 +8,15 @@ export default function renderPaginationLinks(page) {
 
   const paginationWrapper = createElement('div', '', 'pagination-wrapper');
 
+  if (currentPage === 1) {
+    const firstPaginationElement = createElement('span', 'First Page', 'pagination-link current-pagination-link first-pagination-link');
+    paginationWrapper.append(firstPaginationElement);
+  } else {
+    const firstPaginationLink = createElement('a', 'First Page', 'pagination-link first-pagination-link');
+    firstPaginationLink.href = './posts.html?page=1';
+    paginationWrapper.append(firstPaginationLink);
+  }
+
   for (let i = 1; i <= pages; i++) {
     if (currentPage === i) {
       const paginationElement = createElement('span', i, 'pagination-link current-page-link');
@@ -17,6 +26,15 @@ export default function renderPaginationLinks(page) {
       paginationLink.href = './posts.html?page=' + i;
       paginationWrapper.append(paginationLink);
     }
+  }
+
+  if (currentPage === pages) {
+    const lastPaginationElement = createElement('span', 'Last Page', 'pagination-link last-pagination-link current-page-link');
+    paginationWrapper.append(lastPaginationElement);
+  } else {
+    const lastPaginationLink = createElement('a', 'Last Page', 'pagination-link last-pagination-link');
+    lastPaginationLink.href = './posts.html?page=' + pages;
+    paginationWrapper.append(lastPaginationLink);
   }
 
   return paginationWrapper;
